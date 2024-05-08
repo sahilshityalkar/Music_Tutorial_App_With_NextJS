1c86070cc172be731c2e1c1ae90c052cbc7d1a51
sahil <sahilshityalkar05@gmail.com>
2024-05-08 15:30:00 +0530

    add Aceternity UI Navbar

--- src/app/myfiles/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Firstfile(){
-    return (
-        <h1 className="text-3xl text-center mt-8">first file code</h1>
-    )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-2xl text-center">code with sahil</h1>
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+      <h1 className="text-2xl text-center">sahil or code</h1>
+    </main>
   );
 }
